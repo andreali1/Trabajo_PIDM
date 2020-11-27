af6d98a5e5c4263a7534c3a81ab7dc2e6883078d
JhosmelPCB <jhosmelpc@gmail.com>
2020-11-26 19:59:05 -0500

    Subiendo Proyecto

---
+++ app/src/main/java/com/copypaste/jzk/camerabarcodescanner/MainActivity.java
@@ -0,0 +1,52 @@
+package com.copypaste.jzk.camerabarcodescanner;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnScanner;
+    private TextView tvBarCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnScanner = findViewById(R.id.btnScanner);
+        tvBarCode = findViewById(R.id.tvBarCode);
+
+        btnScanner.setOnClickListener(mOnClickListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null)
+            if (result.getContents() != null){
+                tvBarCode.setText("El código de barras es:\n" + result.getContents());
+            }else{
+                tvBarCode.setText("Error al escanear el código de barras");
+            }
+    }
+
+    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btnScanner:
+                    new IntentIntegrator(MainActivity.this).initiateScan();
+                    break;
+            }
+        }
+    };
+}
